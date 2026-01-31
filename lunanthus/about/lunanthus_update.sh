7d0ba76268cdd40ec7e6457d22ebf2fdc2984276
瑞井 箕月 <6161.k.iga@gmail.com>
2026-01-31 19:27:46 +0900

    Refactor lunanthus_update.sh for improved clarity
    Refactor script to use PATHS array instead of WHITELIST for sparse checkout. Simplify remote handling and checkout process.

--- lunanthus/about/lunanthus_update.sh
+++ lunanthus/about/lunanthus_update.sh
@@ -3,26 +3,19 @@
 REPO_URL="https://github.com/MizuiMiduki/Lunanthus.git"
 REMOTE_NAME="lunanthus_origin"
 
-WHITELIST=("lunanthus/spa/" "lunanthus/pwa/sw.js" "lunanthus/router.js" "lunanthus/about/lunanthus_about.json")
+PATHS=(
+  "lunanthus/spa/"
+  "lunanthus/pwa/sw.js"
+  "lunanthus/router.js"
+ "lunanthus/about/lunanthus_about.json"
+)
 
-if ! git remote | grep -q "$REMOTE_NAME"; then
-    git remote add $REMOTE_NAME $REPO_URL
+if ! git remote get-url lunanthus_origin >/dev/null 2>&1; then
+  git remote add lunanthus_origin https://github.com/MizuiMiduki/Lunanthus.git
 fi
 
 git fetch $REMOTE_NAME
 
-git config core.sparseCheckout true
-
-echo "" > .git/info/sparse-checkout
-for item in "${WHITELIST[@]}"; do
-    echo "$item" >> .git/info/sparse-checkout
-done
-
-echo "Applying updates to: ${WHITELIST[*]}"
-git checkout $REMOTE_NAME/main -- ${WHITELIST[@]}
-
-git config core.sparseCheckout false
-echo "*" > .git/info/sparse-checkout
-git read-tree -mu HEAD
+git checkout $REMOTE_NAME/main -- "${PATHS[@]}"
 
 echo "Update complete!"
